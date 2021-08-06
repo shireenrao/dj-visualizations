71bb87afb9e73f9c238bd381af8c199f518d8763
Srinivas Nyayapati <shireenrao@gmail.com>
2021-08-06 12:53:51 -0400

    update sales to generate transaction_id

--- sales/models.py
+++ sales/models.py
@@ -5,6 +5,8 @@ from customers.models import Customer
 from products.models import Product
 from profiles.models import Profile
 
+from .utils import generate_code
+
 
 class Position(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
@@ -23,7 +25,7 @@ class Position(models.Model):
 class Sale(models.Model):
     transaction_id = models.CharField(max_length=12, blank=True)
     positions = models.ManyToManyField(Position)
-    total_price = models.FloatField(blank=True)
+    total_price = models.FloatField(blank=True, null=True)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     salesman = models.ForeignKey(Profile, on_delete=models.CASCADE)
     created = models.DateTimeField(blank=True)
@@ -34,7 +36,7 @@ class Sale(models.Model):
 
     def save(self, *args, **kwargs):
         if self.transaction_id == "":
-            self.transaction_id = ""
+            self.transaction_id = generate_code()
         if self.created is None:
             self.created = timezone.now()
         return super().save(*args, **kwargs)
