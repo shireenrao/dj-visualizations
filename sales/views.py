41fe3441695d3074ee0d143b4462439f5b77e4ad
Srinivas Nyayapati <shireenrao@gmail.com>
2021-08-07 11:36:25 -0400

    display dataframe on template

--- sales/views.py
+++ sales/views.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from django.shortcuts import render
 from django.views.generic import DetailView, ListView
 
@@ -6,13 +7,23 @@ from .models import Sale
 
 
 def home_view(request):
+    sales_df = None
     form = SalesSearchForm(request.POST or None)
     if request.method == "POST":
         date_from = request.POST.get("date_from")
         date_to = request.POST.get("date_to")
-        chart_type = request.POST.get("chart_type")
-        print(date_from, date_to, chart_type)
-    context = {"form": form}
+        # chart_type = request.POST.get("chart_type")
+        qs = Sale.objects.filter(
+            created__date__lte=date_to, created__date__gte=date_from
+        )
+        if len(qs) > 0:
+            sales_df = pd.DataFrame(qs.values())
+
+            sales_df = sales_df.to_html()
+            print(sales_df)
+        else:
+            print("no data")
+    context = {"form": form, "sales_df": sales_df}
     return render(request, "sales/home.html", context)
 
 
