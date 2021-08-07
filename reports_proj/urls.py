4d48ef03f52d1d1c6d0127e66e854e5e020704ca
Srinivas Nyayapati <shireenrao@gmail.com>
2021-08-07 10:12:44 -0400

    Adding Sales view

--- reports_proj/urls.py
+++ reports_proj/urls.py
@@ -20,6 +20,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("sales.urls", namespace="sales")),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- sales/views.py
+++ sales/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home_view(request):
+    hello = "hello from view"
+    return render(request, "sales/main.html", {"hello": hello})
