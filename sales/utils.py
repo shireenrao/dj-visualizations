87a53a5c35c855b4d928fe4e51e71d45f2381620
Srinivas Nyayapati <shireenrao@gmail.com>
2021-08-08 09:37:12 -0400

    Add all dataframes for position and sales

--- sales/utils.py
+++ sales/utils.py
@@ -1,6 +1,19 @@
 import uuid
 
+from customers.models import Customer
+from profiles.models import Profile
+
 
 def generate_code():
     code = str(uuid.uuid4()).replace("-", "")[:12]
     return code
+
+
+def get_customer_from_id(val):
+    cust = Customer.objects.get(id=val)
+    return cust.name
+
+
+def get_salesman_from_id(val):
+    salesman = Profile.objects.get(id=val)
+    return salesman.user.username
--- sales/views.py
+++ sales/views.py
@@ -4,26 +4,76 @@ from django.views.generic import DetailView, ListView
 
 from .forms import SalesSearchForm
 from .models import Sale
+from .utils import get_customer_from_id, get_salesman_from_id
 
 
 def home_view(request):
     sales_df = None
+    positions_df = None
+    merged_df = None
+    df = None
     form = SalesSearchForm(request.POST or None)
     if request.method == "POST":
         date_from = request.POST.get("date_from")
         date_to = request.POST.get("date_to")
         # chart_type = request.POST.get("chart_type")
-        qs = Sale.objects.filter(
+        sale_qs = Sale.objects.filter(
             created__date__lte=date_to, created__date__gte=date_from
         )
-        if len(qs) > 0:
-            sales_df = pd.DataFrame(qs.values())
+        if len(sale_qs) > 0:
+            sales_df = pd.DataFrame(sale_qs.values())
+            sales_df["customer_id"] = sales_df["customer_id"].apply(
+                get_customer_from_id
+            )
+            sales_df["salesman_id"] = sales_df["salesman_id"].apply(
+                get_salesman_from_id
+            )
+            sales_df.rename(
+                columns={
+                    "customer_id": "customer",
+                    "salesman_id": "salesman",
+                    "id": "sales_id",
+                },
+                inplace=True,
+            )
+            sales_df["created"] = sales_df["created"].apply(
+                lambda x: x.strftime("%Y-%m-%d")
+            )
+            sales_df["updated"] = sales_df["updated"].apply(
+                lambda x: x.strftime("%Y-%m-%d")
+            )
 
+            positions_data = []
+            for sale in sale_qs:
+                for pos in sale.get_positions():
+                    obj = {
+                        "position_id": pos.id,
+                        "product": pos.product.name,
+                        "quantity": pos.quantity,
+                        "price": pos.price,
+                        "sales_id": pos.get_sales_id(),
+                    }
+                    positions_data.append(obj)
+
+            positions_df = pd.DataFrame(positions_data)
+            merged_df = pd.merge(sales_df, positions_df, on="sales_id")
+
+            df = merged_df.groupby("transaction_id", as_index=False)["price"].agg("sum")
+            df = df.to_html()
+            positions_df = positions_df.to_html()
             sales_df = sales_df.to_html()
-            print(sales_df)
+            merged_df = merged_df.to_html()
+            # print(sales_df)
+            # print(positions_df)
         else:
             print("no data")
-    context = {"form": form, "sales_df": sales_df}
+    context = {
+        "form": form,
+        "sales_df": sales_df,
+        "positions_df": positions_df,
+        "merged_df": merged_df,
+        "df": df,
+    }
     return render(request, "sales/home.html", context)
 
 
