a340b4f88389eb257ee1fb13b29937d53994e83c
Srinivas Nyayapati <shireenrao@gmail.com>
2021-08-09 09:04:01 -0400

    Add image to modal form

--- sales/static/sales/home.js
+++ sales/static/sales/home.js
@@ -2,6 +2,7 @@
 
 const reportBtn = document.getElementById("report-btn")
 const img = document.getElementById("img")
+const modelBody = document.getElementById("modal-body")
 
 // console.log(reportBtn)
 // console.log(img)
@@ -9,3 +10,9 @@ const img = document.getElementById("img")
 if (img) {
     reportBtn.classList.remove("not-visible")
 }
+
+reportBtn.addEventListener('click', () => {
+    console.log("clicked")
+    img.setAttribute("class", "w-100")
+    modelBody.prepend(img)
+})
